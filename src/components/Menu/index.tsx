88f271e1a47330258a9bba89f52da4d7be6d56c1
ufogreys <45027262+ufogreys@users.noreply.github.com>
2022-05-13 20:08:12 -0300

    Update index.tsx

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -25,7 +25,7 @@ const Menu = (props) => {
       setLang={setSelectedLanguage}
       cakePriceUsd={cakePriceUsd.toNumber()}
       links={config}
-      priceLink="https://www.coingecko.com/en/coins/taco-finance"
+      priceLink="https://www.coingecko.com/en/coins/binance-usd"
       {...props}
     />
   )
